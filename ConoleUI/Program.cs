8f0a367300872a66e30e2c1712a6b616301a0159
obird <obird@ATE>
2022-10-18 00:25:05 +0300

    Add project files.

---
+++ ConoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+ProductManager productManager= new ProductManager( new InMemoryPrpductDal());
+foreach (var product in productManager.GetAll())
+
+{
+    Console.WriteLine(product.ProductName);
+}
